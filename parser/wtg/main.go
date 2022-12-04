4d76081a7c92eeb3ae309f08942990711c2e826b
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2022-12-04 12:59:03 +0100

    feat: graph

--- parser/wtg/main.go
+++ parser/wtg/main.go
@@ -7,49 +7,9 @@ import (
 	"os"
 	"regexp"
 
-	"gonum.org/v1/gonum/graph"
 	"gonum.org/v1/gonum/graph/encoding/dot"
 	"gonum.org/v1/gonum/graph/simple"
 )
-
-type node struct {
-	id    int64
-	label string
-}
-
-func (n *node) ID() int64 {
-	return n.id
-}
-
-type edge struct {
-	from       *node
-	to         *node
-	visibility int
-}
-
-// From returns the from node of the edge.
-func (e *edge) From() graph.Node {
-	return e.from
-}
-
-// To returns the to node of the edge.
-func (e *edge) To() graph.Node {
-	return e.to
-}
-
-// ReversedEdge returns the edge reversal of the receiver
-// if a reversal is valid for the data type.
-// When a reversal is valid an edge of the same type as
-// the receiver with nodes of the receiver swapped should
-// be returned, otherwise the receiver should be returned
-// unaltered.
-func (e *edge) ReversedEdge() graph.Edge {
-	return &edge{
-		from:       e.to,
-		to:         e.from,
-		visibility: e.visibility,
-	}
-}
 
 func main() {
 
@@ -58,13 +18,11 @@ func main() {
 	var link = regexp.MustCompile(`^(.*) (-+) (.*)$`)
 
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("graph G {")
 	for scanner.Scan() {
 		elements := link.FindStringSubmatch(scanner.Text())
 		if len(elements) != 4 {
 			log.Fatal("bad entry", scanner.Text())
 		}
-		fmt.Printf(`"%v" -- "%v" [minlen=%v]`+"\n", elements[1], elements[3], len(elements[2]))
 		if _, ok := inventory[elements[1]]; !ok {
 			inventory[elements[1]] = &node{
 				id:    int64(len(inventory)),
@@ -84,7 +42,6 @@ func main() {
 		})
 	}
 
-	fmt.Println("}")
 	g := simple.NewDirectedGraph()
 	for _, n := range inventory {
 		g.AddNode(n)
@@ -92,7 +49,7 @@ func main() {
 	for _, e := range edgeInventory {
 		g.SetEdge(e)
 	}
-	b, err := dot.Marshal(g, "sample", " ", " ")
+	b, err := dot.Marshal(g, "sample", "", "\t")
 	if err != nil {
 		log.Fatal(err)
 	}
