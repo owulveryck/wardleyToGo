649302576dde8306747816fe802a3556fed3b23c
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2023-01-23 18:01:47 +0100

    feat: TextArea for the long components

--- components/wardley/component_test.go
+++ components/wardley/component_test.go
@@ -1,7 +1,11 @@
 package wardley
 
 import (
+	"bytes"
+	"encoding/xml"
 	"image"
+	"image/color"
+	"testing"
 
 	"github.com/owulveryck/wardleyToGo/components"
 )
@@ -15,4 +19,55 @@ func ExampleComponent_Draw() {
 		Placement:      image.Pt(100, 50),
 	}
 	c.Draw(im, im.Bounds(), im, image.Point{})
+}
+
+func TestComponent_MarshalSVG(t *testing.T) {
+	c := &Component{
+		id:                 0,
+		Placement:          image.Point{50, 50},
+		Label:              "label",
+		LabelPlacement:     image.Point{components.UndefinedCoord, components.UndefinedCoord},
+		Type:               PipelineComponent,
+		RenderingLayer:     0,
+		Configured:         true,
+		EvolutionPos:       0,
+		Color:              color.Gray{},
+		AbsoluteVisibility: 0,
+		PipelinedComponents: map[string]*Component{
+			"p1": {
+				id:                  0,
+				Placement:           image.Point{20, 50},
+				Label:               "p1",
+				LabelPlacement:      image.Point{components.UndefinedCoord, components.UndefinedCoord},
+				Type:                0,
+				RenderingLayer:      0,
+				Configured:          true,
+				EvolutionPos:        0,
+				Color:               color.Gray{},
+				AbsoluteVisibility:  0,
+				PipelinedComponents: nil,
+			},
+			"p2": {
+				id:                  0,
+				Placement:           image.Point{70, 50},
+				Label:               "p2",
+				LabelPlacement:      image.Point{components.UndefinedCoord, components.UndefinedCoord},
+				Type:                0,
+				RenderingLayer:      0,
+				Configured:          true,
+				EvolutionPos:        0,
+				Color:               color.Gray{},
+				AbsoluteVisibility:  0,
+				PipelinedComponents: nil,
+			},
+		},
+	}
+	var output bytes.Buffer
+	e := xml.NewEncoder(&output)
+	canvas := image.Rect(0, 0, 100, 100)
+	err := c.MarshalSVG(e, canvas)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(output.String())
 }
